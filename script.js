40a88b9449bcf871fcb0c6c083b1d03b550c08cf
SPlinterRed <131634018+SPlinterRed@users.noreply.github.com>
2024-11-28 21:47:03 +0800

    Fight mechanics nlng

--- script.js
+++ script.js
@@ -1,14 +1,223 @@
+class User {
+    constructor(name, hp, currency, equipped, defaultpng) {
+        this.name = name;
+        this.hp = hp;
+        this.currency = currency;
+        this.equipped = equipped;
+        this.defaulpng = defaultpng;
+    }
+}
+
+class Mob {
+    constructor(name, hp, moveset) {
+        this.name = name;
+        this.hp = hp;
+        this.moveset = moveset;
+    }
+}
+
+class Weapon {
+    constructor(type, name, unlocked, moveset) {
+        this.type = type;
+        this.name = name;
+        this.unlocked = unlocked;
+        this.refinement = 1.00;
+        this.moveset = moveset;
+        this.finaldmg =this.moveset.damage * this.refinement;
+    }
+
+    
+}
+
+class Moves {
+    constructor(name, damage, png) {
+        this.name = name;
+        this.damage = damage;
+        this.sprite = png;
+    }
+}
+
+//-------------------WEAPON LIST--------------------------------------------------------------
+const weaponlist = [
+    new Weapon("sword", "Default Blade", true, [
+        new Moves("Focus Slash", 50, ".png"),
+        new Moves("Quick Stab", 20, ".png"),
+        new Moves("Mach Drain", 10, ".png")
+    ]),
+    new Weapon("sword", "Black Blade", false, [
+        new Moves("Bad Omen", 100, ".png"),
+        new Moves("Night Slash", 120, ".png"),
+        new Moves("Equinox", 200, ".png")
+    ])
+];
+
+function display_items() {
+    const targetlist = document.getElementById("prizeBox");
+    targetlist.innerHTML = "";
+
+    for (let i = 0; i < weaponlist.length; i++) {
+        const weap = document.createElement('li');
+        weap.innerHTML = `${weaponlist[i].name} | ${weaponlist[i].type}`;
+        targetlist.appendChild(weap);
+    }
+}
+
+//------------USER LIST------------------------
+const userlist = [
+    new User("Rin", 1000, 1, weaponlist[0], defaultpng("sword"))
+]
+
+//------------------- MOB LIST ------------------------
+const mobs = [
+    new Mob("Estafallacy", 500, [
+        new Moves("Toxic Spit", 50, ".png"),
+        new Moves("Light Gas", 60, ".png"),
+        new Moves("Mind Control", 100, ".png")
+    ]),
+
+    new Mob("Enigma", 5100, [
+        new Moves("Dark Matter", 150, ".png"),
+        new Moves("Soul Drag", 100, ".png"),
+        new Moves("Corruption", 200, ".png")
+    ])
+]
+
+//--------------PNG HANDLER-------------------
+function defaultpng(weaponstype) {
+    if (weaponstype == "sword") {
+        return "DefaultSword"
+    }
+    if (weaponstype == "bow") {
+        return "DefaultBow"
+    }
+    if (weaponstype == "spear") {
+        return "DefaultSpear"
+    }
+    if (weaponstype == "orb") {
+        return "DefaultOrb"
+    }
+    if (weaponstype == "knuckles") {
+        return "DefaultKnuckles"
+    }
+}
+//-----------------------WEAPON GACHA------------------------------
+function gacha() {
+    const result = Math.floor(Math.random() * weaponlist.length);
+    const resultdiv = document.getElementById('gacharesult');
+    resultdiv.innerHTML = '';
+
+    const prize = document.createElement("h1");
+    prize.innerText = `You won ${weaponlist[result].name}`;
+    resultdiv.appendChild(prize);
+
+//--------------------- refinement debug statement------------
+    const verifyMessage = verify(weaponlist[result].name);
+    const verifyMessageDiv = document.createElement("p");
+    verifyMessageDiv.innerText = verifyMessage;
+    resultdiv.appendChild(verifyMessageDiv);
+
+}
+
+//----------------LOCATOR-------------------
+function locate(username){
+    for(let i = 0; i < userlist.length ; i++){
+        if(userlist[i].name == username){
+            return userlist[i]
+        }
+    }
+    return null;
+}
+//--------------------verify para sa refinement
+function verify(item_name) {
+    for (let i = 0; i < weaponlist.length; i++) {
+        if (weaponlist[i].name === item_name) {
+            if (weaponlist[i].unlocked === false) {
+                weaponlist[i].unlocked = true;
+                return "Weapon Unlocked";
+            } else {
+                weaponlist[i].refinement += 0.2;
+                weaponlist[i].finaldmg = weaponlist[i].moveset.damage * weaponlist[i].refinement;
+                return "Weapon Refinement++";
+            }
+        }
+    }
+}
+
+//------------------equip customization-----------
+
+function getuser(){
+    const username = document.getElementById('username').value;
+    targ = locate(username)
+    menu(targ)
+}
+function menu(current_user){
+    const character = document.getElementById("equip")
+    const charname = document.createElement("h1")
+    charname.innerHTML = ` USER :${current_user.name}
+                           Weapon:${current_user.equipped.name}
+                           <img src ="temp.png" height="100px" width="100px">
+                           <button onclick="changeweapon()">CHANGE WEAPON</button>
+                           <div id="weaponry"></div>
+                           `
+
+    character.appendChild(charname)
+}
+//----------------------Change weapon-------------------
+function changeweapon(){
+    const username = document.getElementById('username').value;
+    targ = locate(username)
+    
+    const weaponry = document.getElementById('weaponry')
+    const buttons = document.createElement('div')
+    for (let i = 0; i < weaponlist.length; i++) {
+        if (weaponlist[i].unlocked === true) {
+          
+            const button = document.createElement('button');
+            button.innerHTML = `Equip ${weaponlist[i].name}`;
+           
+            button.addEventListener('click', function() {
+                equip(targ, weaponlist[i]); 
+            });
+
+            buttons.appendChild(button);
+        }
+    }
+
+    weaponry.innerHTML = '';
+    weaponry.appendChild(buttons);
+}
+
+function equip(user, weapon) {
+    user.equipped = weapon;
+    alert(`You have equipped ${weapon.name}!`);
+}
+
+//------------------------------------------------------
+//---------------------------- FIGHT SECTION -----------------------//
+function getuser2(){
+    const username = document.getElementById('username2').value;
+    targ = locate(username)
+}
+
+
 function swapPage(page) {
     const pageDisplay = document.getElementById('pageDisplay');
     if (page === 'menu') {
         pageDisplay.innerHTML = `
-        <h1>Menu</h1>
-        <p>Dito lagay yung character tapos palit palit ng weapons</p>`;
+        <input type="text" id="username" placeholder="Enter your name">
+        <button onclick="getuser()">Submit</button>
+        
+        <div id = "equip"></div>
+        `
+        
     } else if (page === 'fight') {
         pageDisplay.innerHTML = `
             <div class="fightingDiv">
                 <div class="fightBox">
                     <p>Character</p>
+                    <input type="text" id="username2" placeholder="Enter your name">
+                    <button onclick="getuser2()">Submit</button>
+                    
                 </div>
                 <div class="fightBox">
                     <p>Hahampasin</p>
@@ -18,7 +227,12 @@ function swapPage(page) {
     } else if (page === 'gacha') {
         pageDisplay.innerHTML = `
             <h1>Gacha</h1>
-            <p>Lagay mo dito yung gacha mo</p>
+            <button onclick="display_items()">Show table</button>
+            <button onclick="gacha()">ROLL</button>
+        
+            
+            <ul id="prizeBox"></ul>
+            <div id="gacharesult"></div>
         `;
     }
 }
